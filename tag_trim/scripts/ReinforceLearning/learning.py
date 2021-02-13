e306ded729fcd78c17f1ce82ae9bb34c31268f67
TaisukeOtsuki <taisuke.march4@icloud.com>
2021-02-13 16:53:38 +0900

    p3

--- tag_trim/scripts/ReinforceLearning/learning.py
+++ tag_trim/scripts/ReinforceLearning/learning.py
@@ -42,4 +42,32 @@ if __name__ == "__main__":
         pass
 
 
+class ImagePubliser():
+    def __init__(self):
+        self.image_pub = rospy.Publisher("usb_cam/image", Image, queue_size = 1)
+        self.info_pub = rospy.Publisher("usb_cam/camera_info", CameraInfo, queue_size = 1)
+
+        self.bag = rosbag.Bag(\
+        '/home/taisuke/catkin_ws/cam_bag/1m/2020-09-28-13-32-02.bag')
+        self.bridge = CvBridge()
+        self.t  = 0
+    def load(self):
+        for topic, msg, t in self.bag.read_messages():
+
+            if topic == "/usb_cam/camera_info":
+                #A = msg.P
+                #dist = msg.K
+                A =np.array([[msg.P[0],msg.P[1],msg.P[2]],
+                        [msg.P[3],msg.P[4],msg.P[5]],
+                        [msg.P[6],msg.P[7],msg.P[8]]], dtype=np.uint8)
+                dist = np.array([msg.D[:]], dtype=np.uint8)
+            if topic == "/usb_cam/image_raw":
+                image_raw = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+                #image_ori = cv2.undistort(image_raw, A, dist, None)
+                cv2.imshow("aa",image_raw)
+                dt =(t- self.t)
+                dt = int(dt.to_sec()*10000)
+                if(dt == 0): dt=1
+                cv2.waitKey(dt)
+            self.t = t
 
